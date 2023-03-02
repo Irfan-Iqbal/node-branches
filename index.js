903aee534e36524cbcc476b4d988d579c69a3e92
Irfan <bhattiirfaniqbal@gmail.com>
2023-03-02 16:24:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Addiing
+function sum(num1, num2) {
+    return num1 + num2;
+  }
+console.log(sum(2, 3)); 
+
+
+
+// multiply
+function multiply(num3, num4) {
+    return num3 * num4;
+  }
+  console.log(multiply(2, 3)); // Output: 6
+
+
+// Divide
+function divide(num5, num6) {
+    return num5 / num6;
+  }
+  console.log(divide(4,5))
